b42a1c1deda3fcae8145f8b7cfa9d185c7c30f1a
konanxu <konanxuxi@gmail.com>
2016-03-08 23:53:36 +0800

    项目初始化，创建基本文件

---
+++ WeiBo/WeiBo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeiBo
+//
+//  Created by Konan on 16/3/8.
+//  Copyright © 2016年 Konan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
